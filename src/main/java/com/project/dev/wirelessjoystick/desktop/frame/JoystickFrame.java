41beb4a36f24aea2e8daf00e379649d6c913a89d
Dyson <dysontilano@gmail.com>
2023-01-05 12:21:59 -0500

    Updated license in source code files

--- src/main/java/com/project/dev/wirelessjoystick/desktop/frame/JoystickFrame.java
+++ src/main/java/com/project/dev/wirelessjoystick/desktop/frame/JoystickFrame.java
@@ -1,13 +1,16 @@
 /*
- * @fileoverview {JoystickFrame} se encarga de realizar tareas especificas.
+ * @fileoverview    {JoystickFrame} se encarga de realizar tareas específicas.
  *
- * @version             1.0
+ * @version         2.0
  *
- * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
- * Copyright (C) Dyson Parra
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
  *
- * @History v1.0 --- La implementacion de {JoystickFrame} fue realizada el 31/07/2022.
- * @Dev - La primera version de {JoystickFrame} fue escrita por Dyson A. Parra T.
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementación realizada.
+ * @version 2.0     Documentación agregada.
  */
 package com.project.dev.wirelessjoystick.desktop.frame;
 
--- src/main/java/com/project/dev/wirelessjoystick/desktop/frame/WirelessJoystickFrame.java
+++ src/main/java/com/project/dev/wirelessjoystick/desktop/frame/WirelessJoystickFrame.java
@@ -1,13 +1,16 @@
 /*
- * @fileoverview {WirelessJoystickFrame} se encarga de realizar tareas especificas.
+ * @fileoverview    {WirelessJoystickFrame} se encarga de realizar tareas específicas.
  *
- * @version             1.0
+ * @version         2.0
  *
- * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
- * Copyright (C) Dyson Parra
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
  *
- * @History v1.0 --- La implementacion de {WirelessJoystickFrame} fue realizada el 31/07/2022.
- * @Dev - La primera version de {WirelessJoystickFrame} fue escrita por Dyson A. Parra T.
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementación realizada.
+ * @version 2.0     Documentación agregada.
  */
 package com.project.dev.wirelessjoystick.desktop.frame;
 
